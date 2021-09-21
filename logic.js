8299bacda9f3b3926aae3e9db74e84c25c1d6db0
Tejal-neosoft <tejal.supe@neosoftmail.com>
2021-09-21 19:04:58 +0530

    calculator

--- logic.js
+++ logic.js
@@ -1,19 +1,28 @@
 var result = document.getElementById('calc');
 let history = document.getElementById('display');
 result.value='';
+
+// Getting the input value
 function num(val){
     result.value += val;
-    history.innerHTML+=val;
+    history.innerHTML+=val; //History Log
 }
 
+//Calculating the result
 function calc(){
 result.value=eval(result.value);
-history.innerHTML =  history.innerHTML +" = " +   result.value +"<br> ";
+history.innerHTML =  history.innerHTML +" = " +   result.value +"<br> "; //History Log
 result.value='';
-   
 }
 
+//Clear Display Area
 function clr() {
     result.value='';
+    history.innerHTML+='<br>';//history Log
 }
-
+//BackSpace Operation
+del =()=>{
+    var a = result.value;
+    result.value = a.substr(0,a.length-1);
+    history.innerHTML=result.value;
+}
